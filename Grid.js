0adcffbf7bdaad3307a8fac584e013e433690380
QIZHAO CHEN <2766657C@student.gla.ac.uk>
2023-12-01 23:24:36 +0000

    initialisation

---
+++ Grid.js
@@ -0,0 +1,34 @@
+const GRID_SIZE = 3
+const CELL_SIZE = 20
+const CELL_GAP = 2
+
+export default class Grid {
+    constructor(gridElement) {
+        gridElement.style.setProperty("--grid-size", GRID_SIZE)
+        gridElement.style.setProperty("--cell-size", `${CELL_SIZE}vmin`)
+        gridElement.style.setProperty("--cell-gap", `${CELL_GAP}vmin`)
+        this.cells = createCellElements(gridElement).map((cellElements, index) => {
+            return new Cell(cellElement, index % GRID_SIZE, Math.floor(index / GRID_SIZE))
+        })
+        console.log(this.cells)
+    }
+}
+
+class Cell {
+    constructor(cellElement, x, y) {
+        this.cellElement = cellElement
+        this.x = x
+        this.y = y
+    }
+}
+
+function createCellElements(gridElement) {
+    const cells = []
+    for (let i = 0; i < GRID_SIZE * GRID_SIZE; i++) {
+        const cell = document.createElement("div")
+        cell.classList.add("cell")
+        cells.push(cell)
+        gridElement.append(cell)
+    }
+    return cells
+}
